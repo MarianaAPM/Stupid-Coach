1d4674eadf94474e28500b62e7c04553fdded73a
Mariana Marinho <marianaapmarinho@gmail.com>
2021-02-15 13:22:06 -0300

    challenge complete

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  def ask
+    
+
+  end
+
+  def answer
+    @user_question = params[:question]
+    @answer = ""
+
+    if @user_question == 'I am going to work!'
+      @answer = "Great"
+    elsif @user_question[-1] == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+
